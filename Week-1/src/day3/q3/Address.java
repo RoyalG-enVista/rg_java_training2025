2161d8c915ced3eafc98dd2d2db230ad0d6e2856
RoyalG-enVista <rgondane@envistacorp.com>
2025-01-14 19:58:52 +0530

    da3_4 q3 incomplete

---
+++ Week-1/src/day3/q3/Address.java
@@ -0,0 +1,21 @@
+package day3.q3;
+
+import java.io.*;
+
+// Address class with a non-serializable field "salary"
+class Address implements Serializable {
+    private final String address;
+    transient double salary; // salary is not serializable
+
+    // Constructor for Address class
+    public Address(String address, double salary) {
+        this.address = address;
+        this.salary = salary;
+    }
+
+    // Display function to show the address and salary
+    public void display() {
+        System.out.println("Address: " + address);
+        System.out.println("Salary: " + salary);  // Salary will not be serialized
+    }
+}
---
+++ Week-1/src/day3/q3/Employee.java
@@ -0,0 +1,30 @@
+package day3.q3;
+
+import java.io.Serializable;
+
+// Employee class implements Serializable
+class Employee implements Serializable {
+    private final int id;
+    private final String name;
+    private final Address address;
+
+    // Constructor for Employee class
+    public Employee(int id, String name, Address address) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+    }
+
+    // Getter methods for Employee class
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+}
