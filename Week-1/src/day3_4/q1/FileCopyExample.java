a7a6f8562244b37741c3cf5ee611bd5f441a0741
RoyalG-enVista <rgondane@envistacorp.com>
2025-01-14 18:38:16 +0530

    q1 modified

--- Week-1/src/day3_4/q1/FileCopyExample.java
+++ Week-1/src/day3_4/q1/FileCopyExample.java
@@ -4,8 +4,8 @@ import java.io.*;
 public class FileCopyExample {
     public static void main(String[] args) {
         // Define file paths for source and destination files
-        String sourceFilePath = "Jasprit_Bumrah.jpg"; // Adjust this path
-        String destinationFilePath = "Jasprit_BumrahCopy.jpg"; // Adjust this path
+        String sourceFilePath = "Jasprit_Bumrah.jpg";
+        String destinationFilePath = "Jasprit_BumrahCopy.jpg";
 
         // Copying file using FileInputStream and FileOutputStream
         copyFileUsingStreams(sourceFilePath, destinationFilePath);
@@ -31,7 +31,9 @@ public class FileCopyExample {
 
         } catch (IOException e) {
             System.err.println("An error occurred during file copy: " + e.getMessage());
-        } finally {
+        }
+
+        /*finally {
             // Close resources
             try {
                 if (fis != null) {
@@ -43,7 +45,7 @@ public class FileCopyExample {
             } catch (IOException e) {
                 System.err.println("Error closing resources: " + e.getMessage());
             }
-        }
+        }*/
     }
 }
 
